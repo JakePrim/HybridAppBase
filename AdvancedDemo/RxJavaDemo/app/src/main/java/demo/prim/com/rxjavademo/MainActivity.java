105067b6f476964cd8cf984182f0610be49ad508
prim <prim>
2019-05-09 16:12:42 +0800

    操作符完成

--- AdvancedDemo/RxJavaDemo/app/src/main/java/demo/prim/com/rxjavademo/MainActivity.java
+++ AdvancedDemo/RxJavaDemo/app/src/main/java/demo/prim/com/rxjavademo/MainActivity.java
@@ -1,5 +1,6 @@
 package demo.prim.com.rxjavademo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -35,6 +37,100 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        interval();
+    }
+
+    public void zip1(View view) {
+        zip();
+    }
+
+    public void refush(View view) {
+        concat();
+    }
+
+    public void map1(View view) {
+        map();
+    }
+
+    public void interval1(View view) {
+        interval();
+    }
+
+    public void retryWhen1(View view) {
+        retryWhen();
+    }
+
+    final int count = 3;//重试次数为3次
+    final int delay = 5000;//等待时长 重新链接的时间间隔
+    String cacheToken = null;
+
+    @SuppressLint("CheckResult")
+    private void retryWhen() {
+        Disposable disposable = Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+                Log.e(TAG, "subscribe: 1 - 请求网络");
+                //模拟网络请求
+                if (cacheToken == null) {//模拟网络请求失败
+                    emitter.onError(new NullPointerException());
+                } else {
+                    //模拟网络请求成功
+                    emitter.onNext("网络请求成功");//当调用next 时不会走retryWhen
+                }
+            }
+        }).retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
+            @Override
+            public ObservableSource<?> apply(Observable<Throwable> observable) throws Exception {
+                return observable.zipWith(Observable.range(1, 1 + count), new BiFunction<Throwable, Integer, Warrp>() {
+                    @Override
+                    public Warrp apply(Throwable throwable, Integer integer) throws Exception {
+                        Log.e(TAG, "apply: 合并重试次数和异常信息：" + integer);
+                        return new Warrp(integer, throwable);
+                    }
+                }).flatMap(new Function<Warrp, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(Warrp warrp) throws Exception {
+                        //收到异常信息 且重试次数小于设置的次数
+                        if (warrp.throwable instanceof NullPointerException && (warrp.count < count + 1)) {
+                            Log.e(TAG, "apply: 重试次数：" + warrp.count);
+                            Log.e(TAG, "apply: 收到错误准备重试：" + delay + " ms后开始重试");
+                            if (warrp.count == 2) {//模拟 第二次重试 网络请求正常了
+                                cacheToken = "1212213";
+                            }
+                            //重新链接 请求网络
+                            return Observable.timer(delay, TimeUnit.MILLISECONDS);
+                        }
+                        return Observable.error(warrp.throwable);
+                    }
+                });
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e(TAG, "accept: next - " + o);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.e(TAG, "accept: error - " + throwable);
+                    }
+                });
+    }
+
+
+    public class Warrp {
+        public int count;
+        public Throwable throwable;
+
+        public Warrp(int count, Throwable throwable) {
+            this.count = count;
+            this.throwable = throwable;
+        }
+    }
+
+    private void interval() {
         Observable.interval(1, TimeUnit.SECONDS, Schedulers.newThread())
                 .take(5)//限制 拦截产生事件的数量
                 .subscribe(new Observer<Long>() {
@@ -58,9 +154,6 @@ public class MainActivity extends AppCompatActivity {
                         System.out.println("onComplete");
                     }
                 });
-
-        zip();
-
     }
 
     public void zip() {
@@ -196,9 +289,6 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    public void refush(View view) {
-        concat();
-    }
 
     private class ParaseFunction implements Function<String, MyClass> {
 
