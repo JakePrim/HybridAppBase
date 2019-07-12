90e078eb396661cbc14c2bfac333657f0e0858c4
prim <prim>
2019-07-12 14:27:54 +0800

    nt

--- PrimFastFlutter/flutter_action/lib/category_route.dart
+++ PrimFastFlutter/flutter_action/lib/category_route.dart
@@ -6,11 +6,19 @@ import 'widget/unit.dart';
 
 ///a list of [Category]
 ///
-///
-
+/// this of backgroundColor
 final _backgroundColor = Colors.green[100];
 
-class CategoryRoute extends StatelessWidget {
+class CategoryRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new CategoryPage();
+  }
+}
+
+class CategoryPage extends State<CategoryRoute> {
+  final categorys = <CategoryWidget>[];
+
   static const _categoryNames = <String>[
     'Length',
     'Area',
@@ -21,15 +29,40 @@ class CategoryRoute extends StatelessWidget {
     'Currency'
   ];
 
-  static const _baseColors = <Color>[
-    Colors.teal,
-    Colors.orange,
-    Colors.pinkAccent,
-    Colors.blueAccent,
-    Colors.yellow,
-    Colors.greenAccent,
-    Colors.purpleAccent,
-    Colors.red,
+  static const _baseColors = <ColorSwatch>[
+    ColorSwatch(0xFF6AB7A8, {
+      'highLight': Color(0xFF6AB7A8),
+      'splash': Color(0xFF0ABC9B),
+    }),
+    ColorSwatch(0xFFFFD28E, {
+      'highLight': Color(0xFFFFD28E),
+      'splash': Color(0xFFFFA41C),
+    }),
+    ColorSwatch(0xFFFFB7DE, {
+      'highLight': Color(0xFFFFB7DE),
+      'splash': Color(0xFFF94CBF),
+    }),
+    ColorSwatch(0xFF8899A8, {
+      'highLight': Color(0xFF8899A8),
+      'splash': Color(0xFFA9CAE8),
+    }),
+    ColorSwatch(0xFFEAD37E, {
+      'highLight': Color(0xFFEAD37E),
+      'splash': Color(0xFFFFE070),
+    }),
+    ColorSwatch(0xFF81A56F, {
+      'highLight': Color(0xFF81A56F),
+      'splash': Color(0xFF7CC159),
+    }),
+    ColorSwatch(0xFFD7C0E2, {
+      'highLight': Color(0xFFD7C0E2),
+      'splash': Color(0xFFCA90E5),
+    }),
+    ColorSwatch(0xFFCE9A9A, {
+      'highlight': Color(0xFFCE9A9A),
+      'splash': Color(0xFFF94D56),
+      'error': Color(0xFF912D2D),
+    }),
   ];
 
   List<Unit> _retrieveUnitList(String categoryName) {
@@ -49,9 +82,8 @@ class CategoryRoute extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    final categorys = <CategoryWidget>[];
-
+  void initState() {
+    super.initState();
     for (int i = 0; i < _categoryNames.length; i++) {
       categorys.add(CategoryWidget(
           iconData: Icons.cake,
@@ -59,7 +91,10 @@ class CategoryRoute extends StatelessWidget {
           color: _baseColors[i],
           units: _retrieveUnitList(_categoryNames[i])));
     }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     final listView = Container(
       child: _buildCategoryWidget(categorys),
     );
--- PrimFastFlutter/flutter_action/lib/converter_route.dart
+++ PrimFastFlutter/flutter_action/lib/converter_route.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 import 'widget/unit.dart';
 import 'package:meta/meta.dart';
 
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
+
+  ///Units for this [CategoryWidget]
   final List<Unit> units;
-  final ColorSwatch color;
+
+  ///color for this [CategoryWidget]
+  final Color color;
+
+  ///This [CategoryWidget]'s name
   final String name;
 
   const ConverterRoute(
@@ -13,6 +19,41 @@ class ConverterRoute extends StatelessWidget {
       : assert(units != null),
         assert(color != null),
         assert(name != null);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new ConverterPage();
+  }
+}
+
+class ConverterPage extends State<ConverterRoute>{
+//TODO Set some variables,such as for keeping track of the user's input
+//value and units
+
+//TODO :Determine whether you need to override anything,such as initState()
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //TODO: Add other helper functions. We've given you one,_format()
+
+  ///Clean up conversion;trim trailing zeros, e.g. 5.500 -> 5.5 10.0 -> 10
+  String _format(double conversion){
+    var outputNum = conversion.toStringAsPrecision(7);//??
+    if(outputNum.contains('.') && outputNum.endsWith('0')){
+      var i = outputNum.length - 1;
+      while(outputNum[i] == '0'){
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0,outputNum.length - 1);
+    }
+    if(outputNum.endsWith('.')){
+      return outputNum.substring(0,outputNum.length - 1);
+    }
+    return outputNum;
+  }
 
   AppBar appBar(BuildContext context) {
     return AppBar(
@@ -24,18 +65,20 @@ class ConverterRoute extends StatelessWidget {
             });
       }),
       title: Text(
-        name,
+        widget.name,
         style: Theme.of(context).textTheme.title,
       ),
       centerTitle: true,
-      backgroundColor: color,
+      backgroundColor: widget.color,
       elevation: 1.0,
     );
   }
 
+
+
   @override
   Widget build(BuildContext context) {
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
@@ -57,7 +100,7 @@ class ConverterRoute extends StatelessWidget {
     return Scaffold(
         appBar: appBar(context),
         body: Container(
-          color: color,
+          color: widget.color,
           child: ListView(
             children: unitWidgets,
           ),
