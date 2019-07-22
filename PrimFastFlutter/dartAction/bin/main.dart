b7b00c31299656fab14b2fe4e76599a090d0c114
prim <prim>
2019-07-22 09:57:19 +0800

    nt

---
+++ PrimFastFlutter/dartAction/bin/main.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+  var number;
+  int num2;
+  num num1;
+  double num3;
+  print('number is var:$number,num2 is int:$num2,num2 is num:$num1,num3 is double:$num3');
+  bool bool1;
+  String str;
+  List list;
+  Map map;
+  print('bool1:$bool1,str:$str,list:$list,map:$map');
+}
